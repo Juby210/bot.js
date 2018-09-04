aa7c2da0bfde9725cbc3e4d99b0dde516e35a7ec
hamsterofficial <Lup3@xmcode.pl>
2018-09-04 12:26:09 +0200

    przerobienie komendy help - na piękniejszą :)

--- commands/HelpCommand.js
+++ commands/HelpCommand.js
@@ -1,17 +1,20 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
 const prefix = config.prefix;
+let strona = ("https://botjs.juby.cf/");
+let github = ("https://github.com/Juby210-PL/bot.js");
 
 module.exports.run = async (client, message, args) => {
     var embed = new Discord.RichEmbed;
     embed.setAuthor(`${client.user.username} - Prefix: ${prefix}`, client.user.avatarURL);
     embed.setColor("#0099FF");
-    embed.setTitle(`${prefix}help`);
-    embed.setDescription("Lista komend bota: \n`avatar, userinfo, ban, kick, resetall, renameall, rename, voicekick, voiceban, voiceunban, uptime, github, invite, botinfo, dbl, shorten, urls`");
-    embed.addField("FUNKCJE BETA: \nBot muzyczny:", "`play, search, q, clearqueue, leave, join`");
-    embed.addField("Komendy działające tylko jak bot gra:", "`pause, resume, skip, vol, np`");
-    embed.addBlankField();
+    embed.addField("BASIC (4)", "``avatar, help, userinfo, info``", true);
+    embed.addField("MODERACJA (9)", "``ban, kick, nick, renameall, rename, resetall, voicekick, voiceunban, voiceban``", true);
+    embed.addField("WEB (3)", "``dbl, shorten, urls``", true);
+    embed.addField("MISC (3)", "``uptime, github, invite``", true);
+    embed.addField("MUZYKA (11)", "``play, pause, skip, join, leave, resume, vol, np, clearqueue, search, q``", true);
     embed.addField("Po więcej info o komendach wpisz:", `${prefix}info <komenda>`);
+    embed.addField("Przydatne linki:\n", "[[WWW]](" + strona + ")" + "\n" + "[[GitHub]](" + github + ")", true);
     embed.setFooter("© Juby210 & hamster", client.user.avatarURL);
     embed.setTimestamp()
     message.channel.send(embed);
