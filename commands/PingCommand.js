bdf165c504f36278e6b49bcef6a2d02d9f240fcc
hamsterofficial <Lup3@xmcode.pl>
2018-09-05 11:53:43 +0200

    Dodanie komendy ping ok

---
+++ commands/PingCommand.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+const prefix = config.prefix;
+
+module.exports.run = async (client, message, args) => {
+    const m = await message.channel.send("Poczekaj...");
+    m.edit(`:ping_pong: Ping: **${m.createdTimestamp - message.createdTimestamp}**ms. Gateway (API): **${Math.round(client.ping)}**ms`);
+}    
+
+module.exports.help = {
+    name:"ping",
+    category:"MISC"
+}
