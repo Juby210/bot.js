959d4f75bd5c8c1e1d48676fbb44a107ef1704d1
hamsterofficial <Lup3@xmcode.pl>
2018-09-05 16:11:36 +0200

    ładniejszy styl pinga ;-)

--- commands/PingCommand.js
+++ commands/PingCommand.js
@@ -4,7 +4,7 @@ const prefix = config.prefix;
 
 module.exports.run = async (client, message, args) => {
     const m = await message.channel.send("Poczekaj...");
-    m.edit(`:ping_pong: Ping: **${m.createdTimestamp - message.createdTimestamp}**ms. Gateway (API): **${Math.round(client.ping)}**ms`);
+    m.edit(`:ping_pong: | Ping: **${m.createdTimestamp - message.createdTimestamp}**ms. Gateway (API): **${Math.round(client.ping)}**ms`);
 }    
 
 module.exports.help = {
