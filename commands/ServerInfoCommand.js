1a0990709f11fd8c1ad7b1f7c78d28d7763803dc
hamsterofficial <Lup3@xmcode.pl>
2018-09-10 14:08:54 +0200

    ServerInfo - wyświetlanie wszystkich rol, pokazywanie ile jest kanałów voice, text.

--- commands/ServerInfoCommand.js
+++ commands/ServerInfoCommand.js
@@ -9,14 +9,16 @@ module.exports.run = async (client, message, guild) => {
         return botCount;
       }
 
+    const verificationLevels = ['Brak [None]', 'Niski [Low] (Zweryfikowany Email)', 'Średni [Medium] (Zarejestrowany na discordzie przez 5+ minut)', 'Szalony [Insane] (Zarejestrowany na discordzie przez 10+ minut)', 'Skrajny [Extreme] (Zweryfikowany numer telefonu)'];
+
     let icon = message.guild.iconURL;
     let embed = new Discord.RichEmbed()
     embed.setAuthor(`ServerInfo - ${message.guild.name}`, client.user.avatarURL);
     embed.setThumbnail(icon);
     embed.addField("Użytkowników: ", message.guild.memberCount + " (w tym botów: " + checkBots(message.guild) + ")");
-    embed.addField("Kanałów: [" + message.guild.channels.size + "]", "#niedługo-spis");
-    embed.addField("Role: [" + message.guild.roles.size + "]", "#niedługo-spis");
-    embed.addField("Weryfikacja:", message.guild.verificationLevel);
+    embed.addField("Kanałów: [" + message.guild.channels.size + "]", message.guild.channels.filter(m => m.type === 'voice').size + " - Głosowych\n" + message.guild.channels.filter(m => m.type === 'text').size + " - Textowych");
+    embed.addField("Role: [" + message.guild.roles.size + "]", message.guild.roles.sort((a, b) => a.position - b.position).map(role => role.toString()).slice(1).reverse().join(", "));
+    embed.addField("Weryfikacja:", verificationLevels[message.guild.verificationLevel]);
     embed.addField("Region:", message.guild.region);
     embed.addField("Właściciel:", message.guild.owner);
     embed.addField("Stworzony:", message.guild.createdAt);
