8c42e1210417ba6ae62cdb8b4b1118a1cfeced5c
juby210-PL <juby210@vp.pl>
2018-09-14 18:57:31 +0200

    Dodanie slowmode, którego jeszcze nie ma na discordzie

---
+++ commands/slowmode.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+const prefix = config.prefix;
+const request = require("request");
+var index = require("../index.js");
+
+module.exports.run = async (client, message, args) => {
+    if(!args[0]) {message.channel.send("Nie prawidłowa ilość argumentów!"); return;}
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) {message.channel.send("Ta komenda wymaga uprawnienia `Zarządzanie kanałami`"); return;}
+    if(isNaN(args[0])) {message.channel.send(`Podaj liczbę, a nie ${args[0]}`); return;}
+    if(args[0] >= 121) {message.channel.send("Max. 120 sek"); return;}
+    req("PATCH", `https://discordapp.com/api/channels/${message.channel.id}`, config.token, args[0]).then(body => {
+        if(args[0] == 0) {
+            message.channel.send("Usunięto slowmode!");
+        } else {
+            message.channel.send(`Ustawiono na **${args[0]}sek**!`);
+        }
+    }).catch(err => index.anticrash(message.channel, err));
+}
+
+function req(method, url, token, num) {
+    return new Promise((resolve, reject) => {
+        request({
+            method: method,
+            url: url,
+            json: {rate_limit_per_user: num},
+            headers: {
+                Authorization: "Bot " + token
+            },
+            form: ""
+        }, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body);
+        });
+    }).catch(err => console.log(err));
+}
+
+module.exports.help = {
+    name:"slowmode",
+    category:"MODERACJA"
+}
