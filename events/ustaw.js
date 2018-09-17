afac1bba599db4f1a322887f4705534c36aee150
hamsterofficial <Lup3@xmcode.pl>
2018-09-17 16:47:10 +0200

    tabowanie

--- events/ustaw.js
+++ events/ustaw.js
@@ -6,10 +6,10 @@ const prefix = config.prefix;
 module.exports.status = client => status(client);
 function status(client = new Discord.Client()) {
     try{
-    if (client.guilds.size == 1) {
-        client.user.setPresence({ game: {name: `${prefix}help | 1 serwer`, type: 'WATCHING' }});
-    } else {
-        client.user.setPresence({ game: {name: `${prefix}help | ${client.guilds.size} serwerów`, type: 'WATCHING' }});
-    }
+        if (client.guilds.size == 1) {
+            client.user.setPresence({ game: {name: `${prefix}help | 1 serwer`, type: 'WATCHING' }});
+        } else {
+            client.user.setPresence({ game: {name: `${prefix}help | ${client.guilds.size} serwerów`, type: 'WATCHING' }});
+        }
     } catch(err) {}
 } 
