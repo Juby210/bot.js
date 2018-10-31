65dff0837acb19611f1bb5bf1f1edd3853b1f3d5
juby210-PL <juby210@vp.pl>
2018-10-31 22:19:07 +0100

    emotka = crash

--- commands/obrazki/trump.js
+++ commands/obrazki/trump.js
@@ -4,7 +4,8 @@ const config = require("../../config.json");
 const snek = require("snekfetch");
 
 module.exports.run = async (client, message, args) => {
-    var text = util.polskieliterytoblad(args.join(' '));
+    var text = util.polskieliterytoblad(args.join(' ')).replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g, '');
+    if(!text) return message.channel.send("Podaj jakiś text!");
     snek.get(`http://api.badosz.com/trump?text=${text}`).set({ Authorization: config.tokens.badosz}).then(response => {
         var embed = new Discord.RichEmbed();
         embed.attachFile({attachment: response.body, name: 'tweet.png'});
