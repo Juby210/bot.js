f9a09d1729df41419cb4995a221c542d18cdc584
hamsterofficial <Lup3@xmcode.pl>
2018-11-07 21:32:35 +0100

    apdejt queue

--- commands/muzyka/queue.js
+++ commands/muzyka/queue.js
@@ -5,6 +5,7 @@ var queuefile = require("./f/queue.js");
 module.exports.run = async (client, message, args) => {
     let queue = queuefile.getqueue;
     var song = queue[message.guild.id].song;
+    const player = client.player.get(message.guild.id);
     let tosend = [];
     queue[message.guild.id].songs.forEach((song, i) => { tosend.push({c: i+1, title: song.title, requester: song.requester, length: song.length});});
     if(tosend == []) {
@@ -14,6 +15,7 @@ module.exports.run = async (client, message, args) => {
     const embed = new Discord.RichEmbed;
     embed.setTitle(`Kolejka serwera ${message.guild.name}:`);
     embed.setDescription('Teraz gram: **' + song.title + "**")
+    if (!player.playing) embed.setDescription('Teraz gram: **Brak**');
     tosend.forEach(e => {
         if(e.c >= 26) {embed.setFooter("Cała kolejka się nie zmieściła ;d"); return;}
         var length = util.formatLength(e.length);
