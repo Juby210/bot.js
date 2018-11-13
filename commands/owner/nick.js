219cd4e62c72af901ca4f0e10c8c66fabd2c1a28
hamsterofficial <Lup3@xmcode.pl>
2018-11-13 19:05:45 +0100

    naprawa komendy nick - owner

--- commands/owner/nick.js
+++ commands/owner/nick.js
@@ -4,11 +4,8 @@ const prefix = config.prefix;
 
 module.exports.run = async (client, message, args) => {
 	var text = args.slice(0).join(" ");
-    if(message.author.id != config.settings.ownerid && message.author.id != config.settings.devid) {
+    if(message.author.id != config.settings.ownerid && message.author.id != config.settings.devid) return;
         client.user.setUsername(text).then(message.react("✅"));
-    } else {
-        message.react("❌");
-    } 
 }
 
 module.exports.help = {
