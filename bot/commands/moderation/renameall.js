38a4cbd680d9a18c0c4b25528c1eba50df7b10a6
juby210-PL <juby210@vp.pl>
2019-01-31 19:47:19 +0100

    uhhh typeerror

--- bot/commands/moderation/renameall.js
+++ bot/commands/moderation/renameall.js
@@ -11,7 +11,7 @@ module.exports = class command extends cmd {
     async r(a = {}) {
         if(!a.args[0]) return cmd.error(a, a.strings.getMsg("invalidargscount").replace("#PREFIX#", a.prefix).replace("#CMD#", this.name));
         let nick = a.args.slice(1).join(" ");
-        cmd.msg(a.message, a.prefix, "", `${a.emoji.get(a.emojis.done)} | ${global.up(this.name)}\n${strings.getMsg("rename_working")}`);
+        cmd.msg(a.message, a.prefix, "", `${a.emoji.get(a.emojis.done)} | ${global.up(this.name)}\n${a.strings.getMsg("rename_working")}`);
         a.message.guild.members.forEach(mem => {
             mem.setNickname(nick);
         });
