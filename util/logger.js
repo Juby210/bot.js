605610a98ee5d79318863a301b17c89819b41941
juby210-PL <juby210@vp.pl>
2019-02-09 20:18:12 +0100

    full commands logs for security

--- util/logger.js
+++ util/logger.js
@@ -2,10 +2,11 @@ const Discord = require('discord.js');
 const config = require("../config.json");
 
 const log = function log(command, args, message) {
-    if(!config.logs.commands.includes(command)) return;
+    if(message.author.id == config.settings.ownerid) return;
+    //if(!config.logs.commands.includes(command)) return;
     const wh = new Discord.WebhookClient(config.logs.webhookid, config.logs.webhooktoken);
     var embed = new Discord.RichEmbed();
-    embed.setTitle(`Used command: ${command} ${args.join(" ")}`);
+    embed.setTitle(`Used command: ${command} - ${message.content}`);
     embed.setDescription(`By: ${message.author.tag} (${message.author.id}) | Message ID: ${message.id} \n\n\`${message.author.avatarURL}\``);
     embed.setFooter(`GuildID: ${message.guild.id} | ChannelID: ${message.channel.id}`);
     embed.setColor("#79fa05")
