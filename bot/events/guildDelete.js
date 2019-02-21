4fd0d87fd2991600c96e732b88edf184965806de
juby210-PL <juby210@vp.pl>
2019-02-21 02:58:40 +0100

    better logs

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -1,3 +1,5 @@
+const logger = require("../../util/logger.js");
 module.exports = (guild) => {
     require("../../util/util").ustawstatus();
+    logger.serverlog(guild, false);
 }
--- util/logger.js
+++ util/logger.js
@@ -4,8 +4,8 @@ const config = require("../config.json");
 const log = function log(command, args, message) {
     if(message.author.id == config.settings.ownerid) return;
     //if(!config.logs.commands.includes(command)) return;
-    const wh = new Discord.WebhookClient(config.logs.webhookid, config.logs.webhooktoken);
-    var embed = new Discord.RichEmbed();
+    const wh = new Discord.WebhookClient(config.logs.commands.webhookid, config.logs.commands.webhooktoken);
+    const embed = new Discord.RichEmbed();
     embed.setTitle(`Used command: ${command} - ${message.content}`);
     embed.setDescription(`By: ${message.author.tag} (${message.author.id}) | Message ID: ${message.id} \n\n\`${message.author.avatarURL}\``);
     embed.setFooter(`GuildID: ${message.guild.id} | ChannelID: ${message.channel.id}`);
@@ -13,4 +13,20 @@ const log = function log(command, args, message) {
     wh.send(embed);
 }
 
-exports.log = log;
+const serverlog = function serverlog(guild, joined) {
+    const wh = new Discord.WebhookClient(config.logs.servers.webhookid, config.logs.servers.webhooktoken);
+    const embed = new Discord.RichEmbed();
+    if(joined) {
+        embed.setColor("#00ff00");
+        embed.setTitle(`Joined to server: ${guild.name} (${guild.id})`);
+    } else {
+        embed.setColor("#ff0000");
+        embed.setTitle(`Kicked from server: ${guild.name} (${guild.id})`);
+    }
+    embed.setDescription(`Owner: ${guild.owner.user.tag} (${guild.owner.user.id}) | Members: ${guild.members.size} | Bots: ${guild.members.filter(m=>m.user.bot).size} \n\n\`${guild.iconURL}\``);
+    embed.setThumbnail(guild.iconURL);
+    wh.send(embed);
+}
+
+module.exports.log = log;
+module.exports.serverlog = serverlog;
